9493ccc08b6391a2db90ec636a80c128bb13548f
TABISH ALAM <167228154+itabishalam@users.noreply.github.com>
2025-10-02 11:01:18 +0530

    Day 1: Setup project and created basic 3-column layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="font-bold">
+        {/*Header */}
+        <h1>Component Playground</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="flex h-screen">
+        {/* Sidebar */}
+        <div className="w-1/4 bg-gray-100 p-4">
+          <h2 className="font-bold">Sidebar</h2>
+        </div>
+
+        {/* Preview */}
+        <div className="w-1/2 flex items-center justify-center bg-white border-x">
+          <h2 className="font-bold">Preview</h2>
+        </div>
+
+        {/* Code */}
+        <div className="w-1/4 bg-gray-50 p-4">
+          <h2 className="font-bold">Code</h2>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
 export default App
